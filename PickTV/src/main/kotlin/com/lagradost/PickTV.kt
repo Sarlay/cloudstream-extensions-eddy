03615d9bf20814a846b70282d092c8638233774e
Eddy <kingkama976@gmail.com>
2022-10-20 14:25:30 +0200

    fix erro import

--- PickTV/src/main/kotlin/com/lagradost/PickTV.kt
+++ PickTV/src/main/kotlin/com/lagradost/PickTV.kt
@@ -1,12 +1,10 @@
 package com.lagradost
 
-mport com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
-import org.jsoup.nodes.Element
 import me.xdrop.fuzzywuzzy.FuzzySearch
-import java.util.*
 import kotlin.collections.ArrayList
 
 class PickTV : MainAPI() {
