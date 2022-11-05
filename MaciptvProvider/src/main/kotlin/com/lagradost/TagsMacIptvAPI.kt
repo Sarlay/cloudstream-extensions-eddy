f395e8732ecd0be59037d001ce605452f72d32db
Eddy <kingkama976@gmail.com>
2022-11-05 19:59:01 +0100

    text id account

--- MaciptvProvider/src/main/kotlin/com/lagradost/TagsMacIptvAPI.kt
+++ MaciptvProvider/src/main/kotlin/com/lagradost/TagsMacIptvAPI.kt
@@ -1,62 +1,138 @@
 package com.lagradost
 
-import com.lagradost.cloudstream3.AcraApplication.Companion.getKey
-import com.lagradost.cloudstream3.AcraApplication.Companion.setKey
+import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import com.lagradost.cloudstream3.R
-import com.lagradost.cloudstream3.syncproviders.AuthAPI
-import com.lagradost.cloudstream3.syncproviders.InAppAuthAPI
-import com.lagradost.cloudstream3.syncproviders.InAppAuthAPIManager
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.ui.settings.SettingsAccount.Companion.showLoginInfo
+import com.lagradost.cloudstream3.ui.settings.SettingsAccount.Companion.addAccount
+import com.lagradost.cloudstream3.utils.UIHelper.colorFromAttribute
 
-class TagsMacIptvAPI(index: Int) : InAppAuthAPIManager(index) {
-    override val name = "Tags"
-    override val idPrefix = "tagsiptvbox"
-    override val icon = R.drawable.ic_baseline_extension_24
-    override val requiresUsername = false
-    override val requiresPassword = false
-    override val requiresServer = true
-    override val createAccountUrl = ""
 
-    companion object {
-        const val IPTVBOX_USER_KEY1: String = "tagsiptvbox_user"
-    }
+class MacIptvSettingsFragment(
+    private val plugin: Plugin,
+    val maciptvAPI: MacIptvAPI,
+    val tagsmaciptvAPI: TagsMacIptvAPI
+) :
+    BottomSheetDialogFragment() {
 
-    override fun getLatestLoginData(): InAppAuthAPI.LoginData? {
-        return getKey(accountId, IPTVBOX_USER_KEY1)
-    }
-
-    override fun loginInfo(): AuthAPI.LoginInfo? {
-        val data = getLatestLoginData() ?: return null
-        return AuthAPI.LoginInfo(
-            name = data.server?:"MyTags",
-            accountIndex = accountIndex
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val id = plugin.resources!!.getIdentifier(
+            "iptvbox_settings",
+            "layout",
+            "BuildConfig.LIBRARY_PACKAGE_NAME"
         )
+        val layout = plugin.resources!!.getLayout(id)
+        return inflater.inflate(layout, container, false)
     }
 
-    override suspend fun login(data: InAppAuthAPI.LoginData): Boolean {
-        if (data.server.isNullOrBlank()) return false // we require a tags
-        switchToNewAccount()
-        setKey(accountId, IPTVBOX_USER_KEY1, data)
-        registerAccount()
-        initialize()
-        inAppAuths
-
-        return true
+    private fun <T : View> View.findView(name: String): T {
+        val id = plugin.resources!!.getIdentifier(name, "id", "BuildConfig.LIBRARY_PACKAGE_NAME")
+        return this.findViewById(id)
     }
 
-    override fun logOut() {
-        removeAccountKeys()
-        initializeData()
+    private fun getDrawable(name: String): Drawable? {
+        val id =
+            plugin.resources!!.getIdentifier(name, "drawable", "BuildConfig.LIBRARY_PACKAGE_NAME")
+        return ResourcesCompat.getDrawable(plugin.resources!!, id, null)
     }
 
-    private fun initializeData() {
-        val data = getLatestLoginData() ?: run {
-            MacIPTVProvider.tags = null
-            return
-        }
-        MacIPTVProvider.tags = data.server.toString()
+    private fun getString(name: String): String? {
+        val id =
+            plugin.resources!!.getIdentifier(name, "string", "BuildConfig.LIBRARY_PACKAGE_NAME")
+        return plugin.resources!!.getString(id)
     }
 
-    override suspend fun initialize() {
-        initializeData()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val infoView = view.findView<LinearLayout>("iptvbox_info")
+        val infoTextView = view.findView<TextView>("info_main_text")
+        val infoSubTextView = view.findView<TextView>("info_sub_text")
+        //val infoImageView = view.findView<ImageView>("iptvbox_info_imageview")
+
+        infoTextView.text = getString("iptvbox_info_title") ?: "MacIPTV"
+        infoSubTextView.text = getString("iptvbox_info_summary") ?: ""
+      /*  infoImageView.setImageDrawable(getDrawable("iptvbox_question"))
+        infoImageView.imageTintList =
+            ColorStateList.valueOf(view.context.colorFromAttribute(R.attr.white))*/
+
+        val loginView = view.findView<LinearLayout>("iptvbox_login")
+        val loginTextView = view.findView<TextView>("main_text")
+        val loginImageView = view.findView<ImageView>("iptvbox_login_imageview")
+        loginImageView.setImageDrawable(getDrawable("iptvbox"))
+        loginImageView.imageTintList =
+            ColorStateList.valueOf(view.context.colorFromAttribute(R.attr.white))
+
+        infoView.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                println("It's OK")
+            }
+        })
+
+
+        loginTextView.text = view.context.resources.getString(R.string.login_format).format(
+            maciptvAPI.name,
+            view.context.resources.getString(R.string.account)
+        )
+
+        loginView.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val info = maciptvAPI.loginInfo()
+                if (info != null) {
+                    showLoginInfo(activity, maciptvAPI, info)
+                } else {
+                    addAccount(activity, maciptvAPI)
+                }
+            }
+        })
+
+        val infoViewTags = view.findView<LinearLayout>("tags_info")
+        val infoTextViewTags = view.findView<TextView>("tagsinfo_main_text")
+        val infoSubTextViewTags = view.findView<TextView>("tags_sub_text")
+
+        infoTextViewTags.text = getString("tags_info_title") ?: "MacIPTV"
+        infoSubTextViewTags.text = getString("tags_info_summary") ?: ""
+
+
+        val loginViewTags = view.findView<LinearLayout>("tags_login")
+        val loginTextViewTags = view.findView<TextView>("tagsmain_text")
+
+
+        // object : View.OnClickListener is required to make it compile because otherwise it used invoke-customs
+        infoViewTags.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                println("It's OK")
+            }
+        })
+
+
+        loginTextViewTags.text = view.context.resources.getString(R.string.login_format).format(
+            tagsmaciptvAPI.name,
+            view.context.resources.getString(R.string.account)
+        )
+
+        loginViewTags.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val info = tagsmaciptvAPI.loginInfo()
+                if (info?.accountIndex != null) {
+                    showLoginInfo(activity, tagsmaciptvAPI, info)
+                } else {
+                    addAccount(activity, tagsmaciptvAPI)
+                }
+            }
+        })
     }
 }
