a01204e7d9baadf172f594f105f6c9a4ce29dcd9
Eddy <kingkama976@gmail.com>
2022-11-12 20:26:49 +0100

    VoeExtractor fixed

--- EmpireStreamingProvider/src/main/kotlin/com/lagradost/EmpirestreamingProviderPlugin.kt
+++ EmpireStreamingProvider/src/main/kotlin/com/lagradost/EmpirestreamingProviderPlugin.kt
@@ -10,7 +10,6 @@ class EmpirestreamingProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(EmpirestreamingProvider())
-        registerExtractorAPI(VoeExtractorA())
         registerExtractorAPI(StreamSBadd())
 		
 		
